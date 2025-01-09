bb46fc7d1494ff3de4323835083df0a731b27780
Luida Medina <luidamedina@gmail.com>
2025-01-09 20:07:04 -0300

    commit

--- myproject/accounts/admin.py
+++ myproject/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cliente, Desarrollador
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Desarrollador)
+
+
+
+
--- myproject/accounts/models.py
+++ myproject/accounts/models.py
@@ -16,16 +16,16 @@ class Cliente(models.Model):
         return self.name
 
 
-class Reclutador(models.Model):
+class Desarrollador(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nombre")
     email = models.EmailField(max_length=100, unique=True, verbose_name="Correo Electrónico")
-    company = models.CharField(max_length=100, verbose_name="Empresa", blank=True, null=True)
+    rol = models.CharField(max_length=100, verbose_name="Rol", blank=True, null=True)
     date_joined = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Registro")
     is_active = models.BooleanField(default=True, verbose_name="Activo")
 
     class Meta:
-        verbose_name = "Reclutador"
-        verbose_name_plural = "Reclutadores"
+        verbose_name = "Desarrollador"
+        verbose_name_plural = "Desarrolladores"
         ordering = ['-date_joined']
 
     def __str__(self):
