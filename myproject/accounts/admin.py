ecdda0156f6ec6d8c1bb6aac023b8c33a3c1adb7
Luida Medina <luidamedina@gmail.com>
2025-01-09 20:50:01 -0300

    commit

--- myproject/accounts/admin.py
+++ myproject/accounts/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Cliente, Desarrollador
 
 # Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Desarrollador)
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "company"] 
+    ordering = ["name", "company",]   
+@admin.register(Desarrollador)
+class DesarrolladorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "rol"] 
+    ordering = ["name", "rol",]
 
 
 
